212b62e47fb9919a21b28a8da7b85167758b5c19
yhjxhuaw <yhjxhuaww@gmail.com>
2024-09-21 15:16:48 +0800

    Initial Commit

---
+++ hahatest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  hahatest
+//
+//  Created by Xurui Mei on 21/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ hahatest/hahatestApp.swift
@@ -0,0 +1,17 @@
+//
+//  hahatestApp.swift
+//  hahatest
+//
+//  Created by Xurui Mei on 21/9/24.
+//
+
+import SwiftUI
+
+@main
+struct hahatestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
